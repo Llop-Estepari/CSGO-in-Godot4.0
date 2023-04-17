f14c77641710f33ba84c9be43905d56627625a8d
Llop <blackhear12@gmail.com>
2023-04-18 01:59:54 +0200

    Update

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,16 +1,23 @@
 extends CharacterBody3D
 
-@onready var head = $Pivot
-@onready var weapons_manager = $Pivot/Camera3D/weapons_manager
+enum STATE {WALK, AIR}
+
+@onready var head = $Camera_Pivot
+@onready var weapons_manager = $Camera_Pivot/Camera3D/weapons_manager
+
+var cur_state := STATE.WALK
+var new_state := false
 
 #MOVEMENT
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-const ACCEL = 5.0
-const MAX_SPEED = 20
-const JUMP_VELOCITY = 4.0
+const ACCEL = 2.0
+const AIR_ACCEL = ACCEL / 2
+const MAX_SPEED = 8.0
+const MAX_AIR_SPEED = 15.0
+const JUMP_VELOCITY = 8.0
 const FRICTION = 8.0
 
-var direction := Vector3()
+var can_jump := true
 
 #CONFIG
 var mouse_sens = .15
@@ -31,20 +38,42 @@ func _ready():
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("action_esc"): get_tree().quit()
-	$Debug/DEBUG_LABEL.text = str("Direction: ", direction, "\nVelocity: ", velocity, "\nInput(w-s,a-d): ", Input.get_vector("forward", "backward", "left", "right"))
-
-func _movement(speed, delta):
-	if Input.is_action_just_pressed("action_jump") and is_on_floor(): velocity.y = JUMP_VELOCITY
-	#if not is_on_floor(): cur_state = STATE.AIR
-	var direction = get_direction()
-	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
-	get_floor_normal()
+	
+	match cur_state:
+		STATE.WALK:
+			_movement(delta)
+		STATE.AIR:
+			air(delta)
+	
 	move_and_slide()
+	$Debug/DEBUG_LABEL.text = str("State: ", cur_state, " , new: ", new_state,"\nDirection: ", get_direction(), "\nVelocity: ", velocity, "\nIs on floor: ", is_on_floor() ,"\nInput(w-s,a-d): ", Input.get_vector("forward", "backward", "left", "right"))
+
+func _movement(delta):
+	if new_state:
+		new_state = false
+	if Input.is_action_just_pressed("action_jump") and is_on_floor() and can_jump:
+		velocity.y = JUMP_VELOCITY
+	if get_direction():
+		velocity.z += get_direction().z * ACCEL
+		velocity.z = clamp(velocity.z, -MAX_SPEED, MAX_SPEED)
+		velocity.x += get_direction().x * ACCEL
+		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+	velocity.z = lerp(velocity.z, 0.0, .3)
+	velocity.x = lerp(velocity.x, 0.0, .3)
+	
+	if not is_on_floor():
+		change_state(STATE.AIR)
+
+func air(delta):
+	if new_state:
+		new_state = false
+	velocity.y -= gravity * delta
+	if is_on_floor():
+		change_state(STATE.WALK)
+
+func change_state(_state):
+	new_state = true
+	cur_state = _state
 
 func get_direction() -> Vector3:
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
